8ba25be7e1ec41ccd92829967e36fca301225a45
leegakyeong <alaska7132@naver.com>
2020-07-22 00:44:51 +0900

    Add point earning page and user info page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>내 정보</h1>
+    <div>번호: {{ user.phoneNumber }}</div>
+    <div>적립 횟수</div>
+    <div>1차: </div>
+    <div>2차: </div>
+    <div>토큰</div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  props: {
+    user: Object
+  }
+}
+</script>
---
+++ src/views/Earn.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="about">
+    <h1>적립하기</h1>
+    <p>수량(라벨을 다른 데 달아야 하나?)</p>
+    <form @submit.prevent="handleSubmit">
+      <button @click="subtractOne">−</button>
+      <input type="number" v-model="quantity" />
+      <button @click="addOne">+</button>
+      <br />
+      <button type="submit">적립하기</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Earn",
+  data: function() {
+    return {
+      quantity: 1,
+    }
+  },
+  methods: {
+    addOne: function(e) {
+      e.preventDefault() // 왜 이게 template에서는 안 먹히는 거지?
+      this.quantity++
+    },
+    subtractOne: function(e) {
+      e.preventDefault()
+      if (this.quantity > 1) this.quantity--
+    },
+    handleSubmit: function() {
+      console.log('이 부분은 아직 구현이 안 되어 있습니다~~!!')
+    }
+  },
+  props: {
+    user: Object
+  }
+}
+</script>
