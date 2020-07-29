d0a5ba86a380d712e4203e4842b49324d263b676
leegakyeong <alaska7132@naver.com>
2020-07-30 00:28:11 +0900

    Update fake data

--- fakeData.js
+++ fakeData.js
@@ -1,23 +1,9 @@
-// 쿠폰 얻었을 때의 행동도 쓰기!!!!
-// scan에서.
-//
-
-// 쓸 수 없는 쿠폰은 누를 수 없게 막기. 혹시 눌렸을 때도 서버에서 처리하고
-
-// 아 유저 시나리오를 분기를 정확히 다 짜서 검토해 봐야 할 듯....ㅜㅜㅜㅜ
-
-// 아 랭킹 데이터도 받아야 함!!!!
-
-// reward를 v-for를 써서 보여 주려면 유니크한 key가 필요해서
-// reward의 id나 식별 가능한 키 같은 거 같이 받을 것!!!!!!
-
 const fakeData = {
   main: {
     request: null,
     response: {
       data: {
         achievement: 0.5,
-        justEarned: false,
       },
     },
   },
@@ -30,12 +16,26 @@ const fakeData = {
     response: {
       data: {
         achievement: 0.51,
-        justEarned: false,
+        justEarned: false, // 이것도 빼도 될 것 같긴 한데..?!
+        ranking: [
+          {
+            phoneNumber: '01011112222',
+            purchaseQuantity: 6,
+          },
+          {
+            phoneNumber: '01012345678',
+            purchaseQuantity: 5,
+          },
+          {
+            phoneNumber: '01022221111',
+            purchaseQuantity: 4,
+          },
+        ],
         currentUser: {
           phoneNumber: '01012345678',
           purchaseQuantity: {
-            firstRound: 5,
-            secondRound: 4,
+            firstRound: 3,
+            secondRound: 2,
           },
           rewards: [
             {
@@ -58,18 +58,25 @@ const fakeData = {
       },
     },
   },
-  scan: {
+  verify: {
     request: {
       data: {
         phoneNumber: '01012345678',
-        branch: '도서관점', // 생각해 보니까 이걸 까먹었다.... QR코드에 넣을 수도 있지만
-        verificationCode: 'erod3i9.x91r-134mt0c8134t=134t+',
+        branch: '도서관점', // 생각해 보니까 이 정보를 QR코드에 넣을 수도 있겠다
+        purchaseQuantity: 10,
+        verificationCode: 'erod3i9.x91r-134mt0c8134t=134t+', // 아무말임
       },
     },
     response: {
       data: {
         achievement: 0.52,
         justEarned: true,
+        purchaseCount: 3,
+        newReward: {
+          round: 'second',
+          type: 'bogo',
+          status: 'unused'
+        },
       },
     },
   },
