35ff24e179ce52974b91bef53935ce90da98a663
leegakyeong <alaska7132@naver.com>
2021-02-17 08:03:04 +0900

    Update vue.config.js for the deploy settings

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  publicPath: process.env.PUBLIC_PATH,
-  outputDir: path.resolve(__dirname, '../backend/public'),
+  publicPath: '/frontend-mobile/',
+  outputDir: path.resolve(__dirname, './dist'),
 };
